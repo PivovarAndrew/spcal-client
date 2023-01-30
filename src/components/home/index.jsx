a2c5a6f57bee0b85e2e27fb829e90d97d65700aa
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2023-01-31 01:28:54 +0300

    Add sub nav

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './home.module.scss';
+import logo from 'logo.svg';
 
 const Home = () => {
     return (
@@ -7,6 +8,16 @@ const Home = () => {
             <div className={styles.hero}>
                 <h1>Strong in service</h1>
                 <h1>More service, more security</h1>
+            </div>
+
+            <div className={styles.subNav}>
+                    <div className={styles.container}>
+                        <div className={styles.title}>
+                            <h1>SP</h1> <h2>Calibration Suisse</h2>
+                        </div>
+
+                        <img src={logo} alt="logo"></img>
+                    </div>
             </div>
         </div>
     )
