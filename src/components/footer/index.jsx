cc27968648f2fecfbd3a4819a3f34140169019b2
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2023-01-30 12:09:15 +0300

    Create Footer component

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            Copyright @ SP Calibration Suisse GmbH &emsp; | &emsp;  Churerstrasse 47, 8808 Pfäffikon SZ 
+        </footer>
+    )
+}
+
+export default Footer;
