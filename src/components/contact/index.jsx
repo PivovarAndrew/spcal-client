a798085350f7e9fd4480dd3caed7d2b208a58471
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2023-01-31 04:04:38 +0300

    Create contact container

---
+++ src/components/contact/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './contact.module.scss';
+
+const Contact = () => {
+    return (
+        <>
+            <h3>Contact</h3>
+            <div className={styles.container}>
+                
+            </div>
+        </>
+    )
+}
+
+export default Contact;
