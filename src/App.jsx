7e659b7867930e06729c4bbacf3b74e9df571eca
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2023-01-31 04:52:32 +0300

    Add about us page to the routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from 'components/header'
 import Footer from 'components/footer'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from 'components/home';
+import AboutUs from 'components/about_us';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/about_us" element={<AboutUs />} />
         </Routes>
         <Footer />
       </BrowserRouter>
