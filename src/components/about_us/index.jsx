4af8a24cca68465c12e675378bda4d70d669faa5
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2023-01-31 04:52:17 +0300

    Create about us container

---
+++ src/components/about_us/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './about_us.module.scss'
+
+const AboutUs = () => {
+    return (
+        <>
+            <h3>About Us</h3>
+            <div className={styles.container}>
+                
+            </div>
+        </>
+    )
+}
+
+export default AboutUs;
