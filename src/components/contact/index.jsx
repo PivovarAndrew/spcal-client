dca43642467eededc37edb1e202d633336095c00
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2023-01-31 04:19:50 +0300

    Create input form
    Form for user contact information

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -6,7 +6,31 @@ const Contact = () => {
         <>
             <h3>Contact</h3>
             <div className={styles.container}>
-                
+                <div className={styles.inputForm}>
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+
+                    <div className={styles.inputGroup}>
+                        <label for="name">Name</label>
+                        <input type="text" name="name" placeholder="Enter your name" />
+                    </div>
+
+                    <div className={styles.inputGroup}>
+                        <label for="email">Email</label>
+                        <input type="email" name="email" placeholder="Enter your email" />
+                    </div>
+
+                    <div className={styles.inputGroup}>
+                        <label for="company">Company</label>
+                        <input type="text" name="company" placeholder="Enter your company name" />
+                    </div>
+
+                    <div className={styles.inputGroup}>
+                        <label for="comment">Comment</label>
+                        <textarea type="text" name="comment" placeholder="Leave a comment" />
+                    </div>
+
+                    <button type="submit" name="submit" id="send">Send</button>
+                </div>
             </div>
         </>
     )
